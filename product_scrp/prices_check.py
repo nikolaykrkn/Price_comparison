c748b41fa25b88e3ede833d2ff0b833496c8cb32
nikolay <nikolaykrkn@gmail.com>
2017-06-15 11:29:16 +0300

    replaced urllib.request with requests library

--- product_scrp/prices_check.py
+++ product_scrp/prices_check.py
@@ -1,7 +1,10 @@
-import requests
+from urllib.request import urlopen
+from urllib.request import Request
+from urllib.parse import urlencode
 from bs4 import BeautifulSoup
 import time
 from decimal import *
+import codecs
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) '
 			'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
@@ -10,17 +13,22 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) '
 
 def get_price(URL):
 	error_count = 0
-	while True:
-		try:
-			req = requests.get(URL, data={'headers': HEADERS})
+	#while True:
+	#	try:
+	req = Request(URL, data=None, headers=HEADERS)
 
-			soup = BeautifulSoup(req.content, 'html.parser')
-			break
-		except:
-			time.sleep(5)
-			error_count += 1
-			if error_count < 5:
-				return -2.0
+	with urlopen(req) as site:
+				soup = BeautifulSoup(site.read().decode('utf-8'), 'html.parser')
+	#			break
+	#	except:
+	#		time.sleep(5)
+	#		error_count += 1
+	#		if error_count < 5:
+	#			return -2.0
+	htmlfile = 'check.html'
+	with open(htmlfile, 'wb') as output:
+		file = codecs.encode(str(soup), 'utf-8')
+		output.write(file)
 
 	if "prodirectrunning.com" in URL or "wiggle.co.uk" in URL:
 		return soup.select("[itemprop='price']")[0].text
